5ef2c6eef67b8a5d777894aafe6a87fc6ade3c3b
David Konsumer <konsumer@jetboystudio.com>
2016-02-16 15:30:15 -0800

    minor formatting

--- test.js
+++ test.js
@@ -4,13 +4,12 @@ import jsdoctext from './lib.js'
 import fs from 'fs'
 const doc = fs.readFileSync(__dirname + '/test.doc')
 
-
 describe('jsdoctext', () => {
   it('can be tested', () => {
     expect(1 + 1).to.equal(2)
   })
 
   it('should convert the test doc', () => {
-	console.log(jsdoctext(doc.toString()))
+    console.log(jsdoctext(doc.toString()))
   })
 })
